69195addd85b8830c3d69f817e2310be0a6fc377
steve <stephentriphahn@yahoo.com>
2015-04-13 23:43:24 -0400

    added image and started navbar

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res){
-  res.send("hello, world");
+  res.render('index');
 });
 
 app.listen(process.env.PORT || 3000);
